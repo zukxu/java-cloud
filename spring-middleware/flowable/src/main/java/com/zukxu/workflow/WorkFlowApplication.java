3992271a99f399eda6ba3a24ae9c5000fa62f27e
zukxu <zukxu520@163.com>
2021-11-18 15:11:41 +0800

    fix(*): flowable

---
+++ spring-middleware/flowable/src/main/java/com/zukxu/workflow/WorkFlowApplication.java
@@ -0,0 +1,16 @@
+package com.zukxu.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xupu
+ * @Date 2021-11-16 18:55
+ */
+@SpringBootApplication
+public class WorkFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkFlowApplication.class, args);
+    }
+}
