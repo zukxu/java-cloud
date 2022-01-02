b9b59d98c807d9e68bc2163307f996f3173f9e04
zukxu <zukxu520@163.com>
2022-01-02 19:21:03 +0800

    init(*):TreeNode 数据结构

---
+++ demo-tree/src/main/java/com/zukxu/tree/TreeNode.java
@@ -0,0 +1,55 @@
+package com.zukxu.tree;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 树形结构模型类
+ *
+ * @author zukxu
+ * @since 2022-1-2-18:09:32
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TreeNode<T> {
+
+    /**
+     * 节点根数据
+     */
+    private T rootNode;
+
+    /**
+     * 节点内容
+     */
+    private List<TreeNode<T>> childrenNode = new ArrayList<>();
+
+    /**
+     * 获取子节点
+     *
+     * @param dataList 数据集合
+     * @param idName   id字段名
+     * @param pidName  pid字段名
+     *
+     * @return 子节点集合
+     */
+    public List<TreeNode<T>> childrenNode(List<T> dataList, String idName, String pidName) {
+        ConvertTree<T> convertTree = new ConvertTree<>();
+        String idValue = convertTree.getFieldValue(rootNode, idName);
+        List<T> collect = dataList.stream()
+                                  .filter(t -> idValue.equals(convertTree.getFieldValue(rootNode, pidName))).toList();
+        dataList.removeAll(collect);
+        collect.forEach(t -> {
+            TreeNode<T> treeNode = new TreeNode<>();
+            treeNode.setRootNode(t);
+            childrenNode.add(treeNode);
+        });
+        return childrenNode;
+    }
+
+
+}
---
+++ demo-tree/src/test/java/com/example/demotree/DemoTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
