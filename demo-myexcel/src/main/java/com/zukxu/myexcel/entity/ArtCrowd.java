b1585567348aadac5aba711b215979752f062cb9
zukxu <zukxu520@163.com>
2022-01-09 01:25:16 +0800

    fix(*):提示

--- demo-myexcel/src/main/java/com/zukxu/myexcel/entity/ArtCrowd.java
+++ demo-myexcel/src/main/java/com/zukxu/myexcel/entity/ArtCrowd.java
@@ -26,18 +26,30 @@ public class ArtCrowd extends People {
     @ExcelColumn(order = 6, defaultValue = "---")
     private String hobby;
 
+    //导出图片
     //@ExcelColumn(fileType = FileType.IMAGE)
     //private File image;
+
+    //导入图片
     //private InputStream image;
 
+    //超链接
     //@ExcelColumn(linkType= LinkType.URL)
     //private String hyperlink;
+
+    //下拉列表
     //@ExcelColumn(title="下拉列表")
     //private List<String> dropDownList;
+
+    //简单映射
     //@ExcelColumn(title="性别",mapping="0:男,1:女")
     //private String sex;
+
+    //自定义映射
     //@ExcelColumn(writeConverter = MyConverter.class)
     //private String dept;
+
+    //提示
     //@ExcelColumn(prompt = @Prompt(title = "提示", text = "这是我的提示哦"))
     //Long cats;
 
