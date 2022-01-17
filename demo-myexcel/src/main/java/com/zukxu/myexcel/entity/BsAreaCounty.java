78539aded024bea68b1eb998e24c50bd8adf5c3f
zukxu <zukxu520@163.com>
2022-01-17 16:22:56 +0800

    fix(*):excel读取 fix

---
+++ demo-myexcel/src/main/java/com/zukxu/myexcel/entity/BsAreaCounty.java
@@ -0,0 +1,63 @@
+package com.zukxu.myexcel.entity;
+
+public class BsAreaCounty {
+    private String provinceId;
+
+    private String provinceName;
+
+    private String cityId;
+
+    private String cityName;
+
+    private String countyId;
+
+    private String countyName;
+
+    public String getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(String provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCountyId() {
+        return countyId;
+    }
+
+    public void setCountyId(String countyId) {
+        this.countyId = countyId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+}
