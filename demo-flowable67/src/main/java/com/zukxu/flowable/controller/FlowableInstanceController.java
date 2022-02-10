07fcc2df7d0fbcb82c02c81a74cd9f9a39ce85f5
zukxu <zukxu520@163.com>
2022-02-10 17:38:58 +0800

    fix(*):流程实例

--- demo-flowable67/src/main/java/com/zukxu/flowable/controller/FlowableInstanceController.java
+++ demo-flowable67/src/main/java/com/zukxu/flowable/controller/FlowableInstanceController.java
@@ -2,6 +2,8 @@ package com.zukxu.flowable.controller;
 
 import com.zukxu.common.result.R;
 import com.zukxu.flowable.service.FlowableInstanceService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,10 +32,32 @@ public class FlowableInstanceController {
      */
     @PostMapping("/startByProcDefId/{procDefId}")
     public R<?> addNewInstanceById(@PathVariable("procDefId") String procDefId, @RequestBody Map<String, Object> variables) {
-        return flowableInstanceService.addNewInstanceById(procDefId,variables);
+        return flowableInstanceService.addNewInstanceById(procDefId, variables);
     }
+
+    /**
+     * 根据流程定义key启动流程实例
+     *
+     * @param procDefKey 流程定义key
+     * @param variables  流程变量
+     * @return
+     */
     @PostMapping("/startByProcDefKey/{procDefKey}")
     public R<?> addNewInstanceByKey(@PathVariable("procDefKey") String procDefKey, @RequestBody Map<String, Object> variables) {
-        return flowableInstanceService.addNewInstanceByKey(procDefKey,variables);
+        return flowableInstanceService.addNewInstanceByKey(procDefKey, variables);
     }
+
+    @ApiOperation(value = "激活或挂起流程实例")
+    @PostMapping(value = "/updateState")
+    public R<?> updateState(@ApiParam(value = "1:激活,2:挂起", required = true) @RequestParam Integer state,
+                            @ApiParam(value = "流程实例ID", required = true) @RequestParam String instanceId) {
+        return flowableInstanceService.updateState(state, instanceId);
+    }
+
+    @ApiOperation(value = "删除流程实例")
+    @DeleteMapping(value = "/delete")
+    public R<?> delete(@ApiParam(value = "流程实例ID", required = true) @RequestParam String[] ids) {
+        return flowableInstanceService.deleteByIds(ids);
+    }
+
 }
--- demo-flowable67/src/main/java/com/zukxu/flowable/service/FlowableInstanceService.java
+++ demo-flowable67/src/main/java/com/zukxu/flowable/service/FlowableInstanceService.java
@@ -3,10 +3,13 @@ package com.zukxu.flowable.service;
 import com.zukxu.common.result.R;
 import com.zukxu.common.result.RMap;
 import com.zukxu.flowable.FlowServiceFactory;
+import org.flowable.common.engine.api.FlowableObjectNotFoundException;
+import org.flowable.engine.history.HistoricProcessInstance;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * <p>
@@ -55,5 +58,57 @@ public class FlowableInstanceService extends FlowServiceFactory {
             e.printStackTrace();
             return R.fail("流程启动错误");
         }
+    }
+
+    /**
+     * 激活/挂起流程实例
+     *
+     * @param state      状态
+     * @param instanceId 流程实例id
+     * @return
+     */
+    public R<?> updateState(Integer state, String instanceId) {
+        // 激活
+        if (state == 1) {
+            runtimeService.activateProcessInstanceById(instanceId);
+            return R.ok().message("流程实例激活成功");
+        }
+        // 挂起
+        if (state == 2) {
+            runtimeService.suspendProcessInstanceById(instanceId);
+            return R.ok().message("流程实例挂起成功");
+        }
+        return R.fail();
+    }
+
+    public R<?> deleteByIds(String[] ids) {
+        for (String instanceId : ids) {
+            // 查询历史数据
+            HistoricProcessInstance historicProcessInstance = getHistoricProcessInstanceById(instanceId);
+            if (historicProcessInstance.getEndTime() != null) {
+                historyService.deleteHistoricProcessInstance(historicProcessInstance.getId());
+                return R.fail(instanceId + "实例未结束,删除失败！");
+            }
+            // 删除流程实例
+            runtimeService.deleteProcessInstance(instanceId, "删除实例");
+            // 删除历史流程实例
+            historyService.deleteHistoricProcessInstance(instanceId);
+        }
+        return R.ok().message("删除成功");
+    }
+
+    /**
+     * 根据流程id查询历史记录
+     *
+     * @param processInstanceId
+     * @return
+     */
+    private HistoricProcessInstance getHistoricProcessInstanceById(String processInstanceId) {
+        HistoricProcessInstance historicProcessInstance =
+                historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+        if (Objects.isNull(historicProcessInstance)) {
+            throw new FlowableObjectNotFoundException("流程实例不存在: " + processInstanceId);
+        }
+        return historicProcessInstance;
     }
 }
