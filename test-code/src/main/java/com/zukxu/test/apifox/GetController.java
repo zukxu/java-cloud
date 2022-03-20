277a601e8b875f6ed25ba9766d6c7a5fba311baf
zukxu <zukxu520@163.com>
2022-03-20 16:06:05 +0800

    fix(*):请求接口Get传递参数 fix

--- test-code/src/main/java/com/zukxu/test/apifox/GetController.java
+++ test-code/src/main/java/com/zukxu/test/apifox/GetController.java
@@ -1,6 +1,12 @@
 package com.zukxu.test.apifox;
 
+import com.zukxu.test.apifox.dto.PhoneDTO;
+import com.zukxu.test.apifox.dto.UserDTO;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 /**
  * <p>
@@ -28,6 +34,7 @@ public class GetController {
      * get query请求
      *
      * @param url
+     *
      * @return
      */
     @GetMapping("/query/{url}")
@@ -40,6 +47,7 @@ public class GetController {
      * 参数可以为空
      *
      * @param text
+     *
      * @return
      */
     @GetMapping("/params")
@@ -52,6 +60,7 @@ public class GetController {
      * 将传递的name参数赋给变量text
      *
      * @param text
+     *
      * @return
      */
     @GetMapping("/requiredParams")
@@ -59,4 +68,67 @@ public class GetController {
         return "get params请求: " + "传递的参数为: " + text;
     }
 
+    /**
+     * get Map接收参数
+     *
+     * @param params
+     *
+     * @return
+     */
+    @GetMapping("/map")
+    public String map(@RequestParam Map<String, Object> params) {
+        return "get map接收参数: " + "传递的参数为: " + params.get("name") + params.get("age");
+    }
+
+    /**
+     * Get 请求接受一个数组
+     *
+     * @param name
+     *
+     * @return
+     */
+    @GetMapping("/array")
+    public String array(@RequestParam String[] name) {
+        return "get Array接收参数: " + "传递的参数为: " + Arrays.toString(name);
+    }
+
+    /**
+     * Get 请求 对象接收参数
+     *
+     * @param dto
+     *
+     * @return
+     */
+    @GetMapping("/object")
+    public String object(UserDTO dto) {
+        return "get 对象接收参数: " + "传递的参数为: " + dto.toString();
+    }
+
+    /**
+     * Get 请求 对象接收参数 自定义前缀
+     *
+     * @param dto
+     *
+     * @return
+     */
+    @GetMapping("/object/prefix")
+    public String objectPrefix(@ModelAttribute("u") UserDTO dto) {
+        return "get 对象自定义前缀接收参数: " + "传递的参数为: " + dto.toString();
+    }
+
+    @InitBinder
+    private void initBinder(WebDataBinder binder) {
+        binder.setFieldDefaultPrefix("u.");
+    }
+    /**
+     * Get 多个对象传参
+     *
+     * @param dto
+     *
+     * @return
+     */
+    @GetMapping("/object/multi")
+    public String objectMulti(UserDTO dto, PhoneDTO phoneDTO) {
+        return "get 多个对象接收参数: " + "传递的参数为: " + dto.toString()+phoneDTO.toString();
+    }
 }
---
+++ test-code/src/main/java/com/zukxu/test/apifox/dto/PhoneDTO.java
@@ -0,0 +1,10 @@
+package com.zukxu.test.apifox.dto;
+
+import lombok.Data;
+
+@Data
+public class PhoneDTO {
+
+    private String number;
+
+}
