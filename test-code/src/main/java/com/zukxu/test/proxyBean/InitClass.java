17f89e22880580d340736b1877e321c7d273ef4b
zukxu <zukxu520@163.com>
2022-03-30 14:20:46 +0800

    fix(*): 测试配置属性 proxyBeanMethods

---
+++ test-code/src/main/java/com/zukxu/test/proxyBean/InitClass.java
@@ -0,0 +1,36 @@
+package com.zukxu.test.proxyBean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author xupu
+ * @since 2022-03-30 11:55
+ */
+@Component
+public class InitClass implements InitializingBean, ApplicationContextAware {
+
+    ApplicationContext applicationContext;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        MyBean2 myBean2 = this.applicationContext.getBean(MyBean2.class); // 第一行
+        MyBean2 myBean21 = this.applicationContext.getBean(MyBean2.class);  // 第二行
+        MyBean1 myBean1 = this.applicationContext.getBean(MyBean1.class);    // 第三行
+        myBean2.hello();
+        myBean21.hello();
+        myBean1.hello();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
---
+++ test-code/src/main/java/com/zukxu/test/proxyBean/OrderEntity.java
@@ -0,0 +1,14 @@
+package com.zukxu.test.proxyBean;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author xupu
+ * @since 2022-03-30 11:55
+ */
+public class OrderEntity {
+    private String id;
+    private String name;
+}
