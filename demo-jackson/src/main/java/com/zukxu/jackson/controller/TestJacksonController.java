af84b1d55441681693128ca18f71645ff21fa684
zukxu <zukxu520@163.com>
2022-05-25 09:22:06 +0800

    feat(jackson): jackson module

---
+++ demo-jackson/src/main/java/com/zukxu/jackson/controller/TestJacksonController.java
@@ -0,0 +1,18 @@
+package com.zukxu.jackson.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author xupu
+ * @since 2022/5/24 16:44:41
+ */
+@RestController
+@RequestMapping("/test/jackson")
+public class TestJacksonController {
+
+}
