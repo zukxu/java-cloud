9893a97f79192a7de261a91fdccf57596c89c731
zukxu <zukxu520@163.com>
2022-05-26 14:51:21 +0800

    feat(jackson): jackson deserializer

--- demo-jackson/src/main/java/com/zukxu/jackson/controller/JacksonApiDemo.java
+++ demo-jackson/src/main/java/com/zukxu/jackson/controller/JacksonApiDemo.java
@@ -2,10 +2,8 @@ package com.zukxu.jackson.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.zukxu.jackson.controller.bean.TestEntity;
-import org.apache.catalina.mapper.Mapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,13 +19,31 @@ import java.time.LocalDateTime;
  */
 public class JacksonApiDemo {
 
-    public static void main(String[] args) throws IOException {
-        //单个对象序列化
-        TestEntity entity = new TestEntity().setId(12345L).setText("Jackson测试json序列化和反序列化").setCreateTime(LocalDateTime.now());
-        ObjectMapper mapper = new ObjectMapper();
+    private static ObjectMapper mapper;
+
+    static {
+        mapper = new ObjectMapper();
         //开启Jackson对LocalDateTime的支持
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.registerModule(new JavaTimeModule());
+    }
+
+    public static void main(String[] args) throws IOException {
+        serializerObject();
+        deserializerObject();
+    }
+
+    private static void deserializerObject() throws IOException {
+        String jsonStr = "{\"id\":12345,\"text\":\"Jackson测试json序列化和反序列化\",\"createTime\":\"2022-05-26T14:40:56.85\"}";
+        TestEntity testEntity = mapper.readValue(jsonStr, TestEntity.class);
+        TestEntity testEntityFile = mapper.readValue(new File("testEntity.json"), TestEntity.class);
+        byte[] array = mapper.writeValueAsBytes(testEntity);
+        TestEntity testEntityArray = mapper.readValue(array, TestEntity.class);
+    }
+
+    private static void serializerObject() throws IOException {
+        //单个对象序列化
+        TestEntity entity = new TestEntity().setId(12345L).setText("Jackson测试json序列化和反序列化").setCreateTime(LocalDateTime.now());
 
         //对象转字符串：
         String jsonStr = mapper.writeValueAsString(entity);
