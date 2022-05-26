d40173a5a1f0c12b73ad2b30de1f02a6bede2d82
zukxu <zukxu520@163.com>
2022-05-26 14:42:35 +0800

    feat(jackson): jackson api object

--- demo-jackson/src/main/java/com/zukxu/jackson/controller/JacksonApiDemo.java
+++ demo-jackson/src/main/java/com/zukxu/jackson/controller/JacksonApiDemo.java
@@ -1,5 +1,16 @@
 package com.zukxu.jackson.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.zukxu.jackson.controller.bean.TestEntity;
+import org.apache.catalina.mapper.Mapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 /**
  * <p>
  * api demo
@@ -9,4 +20,23 @@ package com.zukxu.jackson.controller;
  * @since 2022/5/25 14:20:36
  */
 public class JacksonApiDemo {
+
+    public static void main(String[] args) throws IOException {
+        //单个对象序列化
+        TestEntity entity = new TestEntity().setId(12345L).setText("Jackson测试json序列化和反序列化").setCreateTime(LocalDateTime.now());
+        ObjectMapper mapper = new ObjectMapper();
+        //开启Jackson对LocalDateTime的支持
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.registerModule(new JavaTimeModule());
+
+        //对象转字符串：
+        String jsonStr = mapper.writeValueAsString(entity);
+        System.out.println(jsonStr);
+        //对象转文件
+        mapper.writeValue(new File("testEntity.json"), entity);
+        //对象转数组
+        byte[] array = mapper.writeValueAsBytes(entity);
+        System.out.println(array);
+    }
+
 }
