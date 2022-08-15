33ee4c2e42e037aa73bfc8700e186f8328dc1abd
zukxu <zukxu520@163.com>
2022-08-15 16:13:55 +0800

    fix(*): jenkins demo

---
+++ demo-jenkins/src/test/java/com/zukxu/demojenkins/DemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zukxu.demojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
