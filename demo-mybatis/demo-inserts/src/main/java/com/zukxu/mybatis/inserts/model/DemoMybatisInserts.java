6ec1953868d4706588c73781e9bd59a1e5d0ddc3
zukxu <zukxu520@163.com>
2022-08-25 11:23:38 +0800

    fix(*): 批量插入

---
+++ demo-mybatis/demo-inserts/src/main/java/com/zukxu/mybatis/inserts/model/DemoMybatisInserts.java
@@ -0,0 +1,35 @@
+package com.zukxu.mybatis.inserts.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DemoMybatisInserts {
+
+    /**
+     *id
+     */
+    private Integer id;
+
+    /**
+     *用户名
+     */
+    private String username;
+
+    /**
+     *密码
+     */
+    private String password;
+
+    /**
+     *工号
+     */
+    private String No;
+
+}
+
