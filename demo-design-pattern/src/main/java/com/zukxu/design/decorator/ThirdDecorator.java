1c7e1bf26a77eaf19da7a7919578e54206c26c05
zukxu <zukxu520@163.com>
2023-02-28 15:49:00 +0800

    fix(*): 装饰器模式 --fix

---
+++ demo-design-pattern/src/main/java/com/zukxu/design/decorator/ThirdDecorator.java
@@ -0,0 +1,31 @@
+package com.zukxu.design.decorator;
+
+import com.zukxu.design.decorator.service.WorkFlowService;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+
+/**
+ * <p>
+ * 三方装饰类
+ * </p>
+ *
+ * @author xupu
+ * @since 2023/2/28 15:33:01
+ */
+@Slf4j
+public class ThirdDecorator implements WorkFlowService {
+
+    private WorkFlowService thirdService;
+
+    public ThirdDecorator(WorkFlowService thirdService) {
+        this.thirdService = thirdService;
+    }
+
+    @Override
+    public String dispatchCSS(HashMap<String, Object> param) {
+        log.info("三方===>省分-派发装饰加强:{}", param);
+        return "三方===>省分-派发装饰加强";
+    }
+
+}
---
+++ demo-design-pattern/src/main/java/com/zukxu/design/decorator/service/WorkFlowService.java
@@ -0,0 +1,26 @@
+package com.zukxu.design.decorator.service;
+
+
+import java.util.HashMap;
+
+
+/**
+ * <p>
+ * WorkFlowService
+ * </p>
+ *
+ * @author xupu
+ * @since 2022-03-31 11:47
+ */
+public interface WorkFlowService {
+
+    /**
+     * 派单
+     *
+     * @param param
+     *
+     * @return
+     */
+    String dispatchCSS(HashMap<String, Object> param);
+
+}
