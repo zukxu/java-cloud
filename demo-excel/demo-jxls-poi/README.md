5673b83f1945ee460d8fc491a5b00332ce533710
xupu <zukxu520@163.com>
2025-04-21 10:23:00 +0800

    [feat]: 新增jxls-poi工具类使用模块

---
+++ demo-excel/demo-jxls-poi/README.md
@@ -0,0 +1,10 @@
+```xml
+<dependency>
+    <groupId>org.jxls</groupId>
+    <artifactId>jxls-poi</artifactId>
+    <version>3.0.0</version>
+</dependency>
+```
+
+### 附录：
+https://jxls.sourceforge.net/getting-started.html
---
+++ demo-excel/demo-jxls-poi/src/main/java/com/zukxu/Main.java
@@ -0,0 +1,15 @@
+package com.zukxu;
+
+/**
+ * <p>
+ * ${END}
+ * </p>
+ *
+ * @author xupu
+ * @since 2024/11/28 11:52:05
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
